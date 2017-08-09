9fdb2e95b03a0a743d9fbc0061638dbdb6e740c3
Regina Imhoff <regina.imhoff@gmail.com>
2017-08-09 11:15:15 -0500

    first tableView working! :tada:

---
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Regina Imhoff on 8/9/17.
+//  Copyright © 2017 Regina Imhoff. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var emojis = ["🙂", "💩", "🌈", "🎉", "😱", "🌟", "✨", "🤖", "🚀", "🏎", "😨", "😭", "😤", "😡", "😎", "🤓", "📱", "🇨🇦", "👻", "👸"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return emojis.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        return cell
+    }
+}
