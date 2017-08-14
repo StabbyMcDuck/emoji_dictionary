7975c0c5de2ba4b0845a63c1a0ceadfcbdad7209
Regina Imhoff <regina.imhoff@gmail.com>
2017-08-14 15:20:05 -0500

    added birth label and category label

--- Emoji Dictionary/DefinitionViewController.swift
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -12,6 +12,8 @@ class DefinitionViewController: UIViewController {
 
     @IBOutlet weak var emojiLabel: UILabel!
     @IBOutlet weak var emojiDefinitionLabel: UILabel!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     
     var emoji = ""
 
@@ -22,44 +24,84 @@ class DefinitionViewController: UIViewController {
         
         if emoji == "🙂" {
             emojiDefinitionLabel.text = "Smiley Face"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "💩" {
             emojiDefinitionLabel.text = "Happy Poop"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "🌈" {
             emojiDefinitionLabel.text = "Rainbow"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Weather"
         } else if emoji == "🎉" {
             emojiDefinitionLabel.text = "Cheers! Ta-da!"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Celebration"
         } else if emoji == "😱" {
             emojiDefinitionLabel.text = "Oh no! Frightful Face!"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "🌟" {
             emojiDefinitionLabel.text = "Shining Star"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Shapes"
         } else if emoji == "✨" {
             emojiDefinitionLabel.text = "Twinkling Stars"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Shapes"
         } else if emoji == "🤖" {
             emojiDefinitionLabel.text = "Robot"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "🚀" {
             emojiDefinitionLabel.text = "Rocket"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Transportation"
         } else if emoji == "🏎" {
             emojiDefinitionLabel.text = "Racecar ZOOM"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Transportation"
         } else if emoji == "😨" {
             emojiDefinitionLabel.text = "Rut-roh Face"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "😭" {
             emojiDefinitionLabel.text = "Crying Face"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "😤" {
             emojiDefinitionLabel.text = "Fuming Face"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "😡" {
             emojiDefinitionLabel.text = "Angry Face"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "😎" {
             emojiDefinitionLabel.text = "Cool Face with Shades"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "🤓" {
             emojiDefinitionLabel.text = "Nerd Face with Glasses"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "📱" {
             emojiDefinitionLabel.text = "iPhone"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Technology"
         } else if emoji == "🇨🇦" {
             emojiDefinitionLabel.text = "Canada"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Flags"
         } else if emoji == "👻" {
             emojiDefinitionLabel.text = "Silly Ghost"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         } else if emoji == "👸" {
             emojiDefinitionLabel.text = "YAAAS QUEEN"
+            birthLabel.text = "Birth Year: 2013"
+            categoryLabel.text = "Category: Faces"
         }
     }
 }
